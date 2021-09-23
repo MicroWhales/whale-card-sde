83feb1e73662f5a68d03e772eb2b7fcd0d5137c7
Bobby <bobbycair@gmail.com>
2021-09-23 18:06:16 +0800

    feat:edited sde shell functionalities

--- sde.sh
+++ sde.sh
@@ -49,8 +49,23 @@ case $1 in
 
     echo "*** Set up complete"
     ;;
+    run-core)
+    cd local/whale-cards-core
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    python3 app.py
+    ;;
+    routes)
+    cd local/whale-cards-core
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    EXPORT FLASK_APP=api.py
+    flask routes
+    ;;
     *)
     echo "Choose script functions - "
     echo "./wmc.sh setup - create local directory and clone relevant repos"
+    echo "./wmc.sh run-core - starts core API server on localhost:5000"
+    echo "./wmc.sh routes - displays available endpoints on core
     ;;
 esac
